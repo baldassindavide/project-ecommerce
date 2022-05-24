c61508ede8974694ae07e0fde26eabd634b340e1
baldassindavide <82998781+baldassindavide@users.noreply.github.com>
2022-05-24 08:19:07 +0200

    Delete project-ecommerce-main directory

--- project-ecommerce-main/e-commerce/logout_manager.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_SESSION["user_ID"] = 6; // guest
-setcookie("logged_in", false, time() + (86400 * 1), "/"); // 86400 = 1 day
-setcookie("current_cart", false, time() + (86400 * 1), "/");
-header("location:index.php");
-
-?>
--- project-ecommerce-main/e-commerce/manager/checkout_manager.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_COOKIE["logged_in"]))
-    header("location:../payment_login.php");
-else
-    header("location:../payment.php");
